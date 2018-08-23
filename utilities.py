7875587f42f1632e71b7b0dd444c454a108a617b
Andrea Dell'Orco <orco@paranoici.org>
2018-08-23 16:01:07 +0200

    split 4d to 3d

--- utilities.py
+++ utilities.py
@@ -8,3 +8,43 @@ def spm_tissues(in_list):
 
     '''
     return in_list[0][0], in_list[1][0], in_list[2][0]
+
+
+# From https://github.com/spinoza-centre/spynoza/blob/master/spynoza/utils.py - MIT License
+def split_4D_to_3D(in_file):
+    """split_4D_to_3D splits a single 4D file into a list of nifti files.
+    Because it splits the file at once, it's faster than fsl.ExtractROI
+    Parameters
+    ----------
+    in_file : str
+        Absolute path to nifti-file.
+    Returns
+    -------
+    out_files : list
+        List of absolute paths to nifti-files.    """
+
+    import nibabel as nib
+    import os
+    import tempfile
+
+    original_file = nib.load(in_file)
+    affine = original_file.affine
+    header = original_file.header
+    dyns = original_file.shape[-1]
+
+    data = original_file.get_data()
+    tempdir = tempfile.gettempdir()
+    fn_base = os.path.split(in_file)[-1][:-7]  # take off .nii.gz
+
+    out_files = []
+    for i in range(dyns):
+        img = nib.Nifti1Image(data[..., i], affine=affine, header=header)
+        opfn = os.path.join(tempdir, fn_base + '_%s.nii.gz' % str(i).zfill(4))
+        nib.save(img, opfn)
+        out_files.append(opfn)
+
+    return out_files
+
+
+Split_4D_to_3D = Function(function=split_4D_to_3D, input_names=['in_file'],
+output_names=['out_files'])
